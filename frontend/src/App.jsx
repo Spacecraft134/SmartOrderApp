d563f4c10508dcc89c98a05fca23a0ac65a71ce9
Rohith Ravikumar <rohithuma123@gmail.com>
2025-06-27 22:05:53 -0500

    Added frontend to folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import MenuManager from "./pages/MenuManager";
+
+function App() {
+  return <MenuManager />;
+}
+
+export default App;
---
+++ frontend/src/pages/MenuManager.jsx
@@ -0,0 +1,7 @@
+export default function MenuManager() {
+  return (
+    <div className="text-red-500">
+      <h1>WORKING</h1>
+    </div>
+  );
+}
